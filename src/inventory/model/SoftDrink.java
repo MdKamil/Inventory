ea27457cb7996fcde21cc135f037d109b3c35df3
mdkamil <a.md.kamil@gmail.com>
2016-07-19 23:43:13 +0530

    Removed two model classes.Made Product class as a Concrete class from abstract class

--- src/inventory/model/SoftDrink.java
+++
@@ -1,9 +0,0 @@
-package inventory.model;
-
-
-public class SoftDrink extends Product {
-
-    public SoftDrink(String productName,int leftInStock,int productRate){
-        super(productName,leftInStock,productRate);
-    }
-}
