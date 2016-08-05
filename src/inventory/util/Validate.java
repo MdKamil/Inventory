53e241c4b81658288fc9e79d826d89af78a74ab4
mdkamil <a.md.kamil@gmail.com>
2016-08-05 08:48:40 +0530

    Fixed Bug in Validate.checkInt method.

--- src/inventory/util/Validate.java
+++ src/inventory/util/Validate.java
@@ -3,7 +3,7 @@ package inventory.util;
 public class Validate {
 
     public static boolean checkInt(String text) {
-        if(text.matches("^[0-9]*$")){
+        if(text.matches("\\d+")){
             return true;
         }else {
             return false;
